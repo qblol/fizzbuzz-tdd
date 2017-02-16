490a7e43a0b1f166ed98f01ef7377b3916e153d5
qblol <iqbal.0809@gmail.com>
2017-02-16 11:41:13 +0700

    add travis.yml

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,10 @@
-const fizzbuzz = { print_array: function(number) { let arr = []; for (let i = 1; i < number + 1; i++) { i % 5 == 0 && i % 3 == 0 ? arr.push("FizzBuzz") : i % 5 == 0 ?  arr.push("Buzz") : i % 3 == 0 ? arr.push("Fizz") : arr.push(i) } return arr } }; module.exports = fizzbuzz;
+const fizzbuzz = {
+  print_array: function(number) {
+    let arr = [];
+    for (let i = 1; i < number + 1; i++) {
+      i % 5 == 0 && i % 3 == 0 ? arr.push("FizzBuzz") : i % 5 == 0 ? arr.push("Buzz") : i % 3 == 0 ? arr.push("Fizz") : arr.push(i)
+    }
+    return arr
+  }
+};
+module.exports = fizzbuzz;
